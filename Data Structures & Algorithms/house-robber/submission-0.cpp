49d0fbf140f52323be833ce6d08e8e78d918fc17
Sarthak Upadhyay <upadhyaysarthak2007@gmail.com>
2026-08-17 22:32:04 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    unordered_map<int,int> map;
+    int dfs(vector<int>& nums,int i) {
+        if(i>=nums.size() || i<0) {
+            map[i]=0;
+            return 0;
+        }
+        if(map.contains(i)) {
+            return map[i];
+        }
+        map[i] = max(nums[i]+dfs(nums,i+2),dfs(nums,i+1));
+        return map[i]; 
+    }
+    int rob(vector<int>& nums) {
+        dfs(nums,0);
+        int ans=0;
+        for(auto& [p,q] : map) {
+            if(q>ans) {
+                ans =q;
+            }
+        }
+        return dfs(nums,0);
+    }
+};
